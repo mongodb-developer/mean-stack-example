94fd397293e94ba0ee8263493acb0ef68883c3a6
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2022-02-24 17:48:44 +0000

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# MEAN Stack Sample Application
+
+This is a simple CRUD application built using the MEAN (MongoDB, Express, Angular, Node.js) stack. 
+
+## How To Run
+
+Set your [Atlas URI connection string](https://docs.atlas.mongodb.com/getting-started/) as a parameter in `server/.env`. Make sure you replace the username and password placeholders with your own credentials.
+
+```
+ATLAS_URI=mongodb+srv://<username>:<password>@sandbox.jadwj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+```
+
+Start the server and client applications:
+```
+npm start
+```
+
+When both applications are built and running, open your browser on http://localhost:4200/.
+
+## Disclaimer
+
+Use at your own risk; not a supported MongoDB product
---
+++ client/src/app/employee.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, Subject, tap } from 'rxjs';
+import { Employee } from './employee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+  private url = 'http://localhost:5200';
+  private employees$: Subject<Employee[]> = new Subject();
+
+  constructor(private httpClient: HttpClient) { }
+
+  private refreshEmployees() {
+    this.httpClient.get<Employee[]>(`${this.url}/employees`)
+      .subscribe(employees => {
+        this.employees$.next(employees);
+      });
+  }
+
+  getEmployees(): Subject<Employee[]> {
+    this.refreshEmployees();
+    return this.employees$;
+  }
+
+  getEmployee(id: string): Observable<Employee> {
+    return this.httpClient.get<Employee>(`${this.url}/employees/${id}`)
+      .pipe(tap(() => this.refreshEmployees()));
+  }
+
+  createEmployee(employee: Employee): Observable<string> {
+    return this.httpClient.post(`${this.url}/employees`, employee, { responseType: 'text' })
+      .pipe(tap(() => this.refreshEmployees()));
+  }
+
+  updateEmployee(id: string, employee: Employee): Observable<string> {
+    return this.httpClient.put(`${this.url}/employees/${id}`, employee, { responseType: 'text' })
+      .pipe(tap(() => this.refreshEmployees()));
+  }
+
+  deleteEmployee(id: string): Observable<string> {
+    return this.httpClient.delete(`${this.url}/employees/${id}`, { responseType: 'text' })
+      .pipe(tap(() => this.refreshEmployees()));
+  }
+}
